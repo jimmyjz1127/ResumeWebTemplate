a53d513c7b8c14240e0b20e4ed156e8a66408994
jimmyjz1127 <jimmyjz1127@gmail.com>
2021-10-30 23:53:30 +0100

    put javascript functions into external files under scripts directory

---
+++ scripts/ericScript.js
@@ -0,0 +1,10 @@
+function openSideBar()
+{
+    document.getElementById("aside").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+function closeSideBar()
+{
+    document.getElementById("aside").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+}
